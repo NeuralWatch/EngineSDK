b17421b6b28f2dbc348e2682432ca8b1ce03dd1b
Trouni Tiet <trouni@gmail.com>
2021-04-04 02:35:50 +0900

    Add tests to all API routes

---
+++ enginesdk/tests/v1/test_api.py
@@ -0,0 +1,60 @@
+import pytest
+from datetime import datetime
+from fastapi.testclient import TestClient
+from enginesdk.api import EngineAPI
+from enginesdk.config import settings
+
+from enginesdk.tests.v1.factories import PredictFactory
+
+
+@pytest.fixture
+def test_predictor():
+    return PredictFactory().mock_predictor()
+
+
+class TestEngineAPI:
+    @pytest.fixture
+    def client(self, test_predictor):
+        api = EngineAPI(test_predictor).api
+
+        return TestClient(api)
+
+    def test_get_healthcheck(self, client):
+        response = client.get("/healthcheck")
+        assert response.status_code == 200
+        data = response.json()
+        assert data["message"] == "Service online"
+        assert data["version"] == settings.revision
+        assert "time" in data
+
+    def test_get_schema(self, client, test_predictor):
+        response = client.get("/v1/schema")
+        assert response.status_code == 200
+        assert response.json() == {
+            "input": test_predictor.Input.schema(),
+            "output": test_predictor.Output.schema(),
+        }
+
+    def test_get_info(self, client, test_predictor):
+        response = client.get("/v1/info")
+        assert response.status_code == 200
+        data = response.json()
+        assert data["schema"] == {
+            "input": test_predictor.Input.schema(),
+            "output": test_predictor.Output.schema(),
+        }
+        assert "engine" in data
+
+    def test_post_predict(self, client, test_predictor):
+        response = client.post(
+            "/v1/predict", json=test_predictor.factory.mock_input().dict()
+        )
+        assert response.status_code == 200
+        for key in test_predictor.Output.__fields__:
+            assert key in response.json()
+
+    def test_post_predict_with_gid(self, client, test_predictor):
+        response = client.post(
+            "/v1/predict/123", json=test_predictor.factory.mock_input().dict()
+        )
+        assert response.status_code == 200
